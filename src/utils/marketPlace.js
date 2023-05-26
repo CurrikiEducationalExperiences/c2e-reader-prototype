0a9ab05e4ca9bf9538d32317182c022934cefa31
muhammadqamar <muhammadqamar111@gmail.com>
2023-05-26 12:05:43 +0500

    income card

--- src/utils/marketPlace.js
+++ src/utils/marketPlace.js
@@ -9,7 +9,7 @@ const MarketPlace = ({
   marketPlaceProducts,
 }) => {
   return (
-    <div className="p-4 w-[241.5px] rounded-lg rounded-s-lg shadow-[0_4px_12px_2px_rgba(81,81,81,0.16)]">
+    <div className="p-4 w-[241.5px] rounded-lg rounded-s-lg shadow-mainshadow text-gray100">
       <h2 className="text-[18px] mb-4">{marketPlaceTitle}</h2>
       <div className="flex items-baseline gap-2 p-1  mb-6 ">
         <img src={Contact} alt="Contact Icon" />
