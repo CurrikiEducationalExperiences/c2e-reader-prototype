0974015cdfe2e7ed7c0533c15713de06f0da819a
muhammadqamar <muhammadqamar111@gmail.com>
2023-05-30 15:54:35 +0500

    fix responsive

--- src/components/dashboard/dashboardSettings/TopMarketPlace.js
+++ src/components/dashboard/dashboardSettings/TopMarketPlace.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import DropdownButton from '../../../utils/dropdownButton';
-import Contact from '../../../assets/images/contact.svg';
-import Folder from '../../../assets/images/folder.svg';
+import DropdownButton from "../../../utils/dropdownButton";
+import Contact from "../../../assets/images/contact.svg";
+import Folder from "../../../assets/images/folder.svg";
 
 const TopMarketPlace = ({ datacard }) => {
-  const data = datacard.filter((item) => item.title === 'TOP MARKETPLACES');
+  const data = datacard.filter((item) => item.title === "TOP MARKETPLACES");
 
   return (
     <>
       <div className="">
         <DropdownButton btntext="MY TOP MARKETPLACES" />
-        <div className="flex items-center gap-4 ">
+        <div className="flex items-center gap-4 flex-wrap ">
           {data?.[0]?.marketPlaceList?.map((item, index) => (
-            <div
-              key={index}
-              className="p-[16px] w-[241.5px] rounded-lg rounded-s-lg shadow-mainshadow text-gray100"
-            >
+            <div key={index} className="p-[16px] w-full md:w-[241.5px] rounded-lg rounded-s-lg shadow-mainshadow text-gray100">
               <h2 className="text-[18px] mb-[16px] ">{item.heading}</h2>
               <div className="flex items-baseline gap-2 p-1 mb-6 ">
                 <img src={Contact} alt="Contact Icon" />
