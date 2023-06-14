4a42029afdeba832af5c3e98fb0b4e76d150396c
muhammadqamar <muhammadqamar111@gmail.com>
2023-06-14 14:35:27 +0500

    responsive sidebar

--- src/pages/builder/index.js
+++ src/pages/builder/index.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import LeftSide from "./leftSide";
-import RightSide from "./rightSide";
-import Workspace from "./workspace";
-import LicenseC2E from "./licenseC2E";
+import LeftSide from './leftSide';
+import RightSide from './rightSide';
+import Workspace from './workspace';
+import LicenseC2E from './licenseC2E';
 
 const Index = () => {
   const [openRoyalties, setOpenRoyalties] = useState(false);
   return (
-    <div className="max-w-full mx-auto w-full pb-8 bg-mainbg pr-6 flex flex-col md:flex-row items-start justify-between gap-[16px] calc-function">
+    <div className="max-w-full mx-auto w-full pb-8 bg-mainbg  flex flex-col md:flex-row items-start justify-between gap-[16px] calc-function">
       {openRoyalties ? (
         <LicenseC2E />
       ) : (
